0a1cb266f7ebbf8736f64184292655a39ba00797
Lee8150951 <Liys9875@outlook.com>
2021-07-28 10:49:22 +0800

    day01

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jacob/Desktop/TestWorkspace/src
+# Build directory: /home/jacob/Desktop/TestWorkspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_topic")
---
+++ build/learning_topic/CMakeFiles/learning_topic_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learning_topic_generate_messages_cpp"
+  "/home/jacob/Desktop/TestWorkspace/devel/include/learning_topic/Person.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/learning_topic_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
